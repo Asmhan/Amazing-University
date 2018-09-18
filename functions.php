b21535a26c731265d05e2980a5e5c1407a035bf9
Asmahan Hussien <loveallah734@yahoo.com>
2018-09-18 21:27:47 +0200

    slide show

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function college_files()
 {
+  wp_enqueue_script('main_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('google_font',  '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font_awesome', '//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('college_main_styles', get_stylesheet_uri(), NULL, microtime());
@@ -15,6 +16,5 @@ function college_features()
   add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'college_features');
-
 
 ?>
