0fec5a66793fab03dd741573150eb488ae0ef807
Asmahan Hussien <loveallah734@yahoo.com>
2018-09-19 18:45:54 +0200

    event and program post relationship

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
         </h2>
         <div class="metabox">
-          <p style="display: flex; align-items: center;">Bosted by <?php echo " "; the_author_posts_link(); echo " ";?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(); ?></p>
+          <p style="display: flex; align-items: center;">Bosted by <?php echo " " . the_author_posts_link() . " ";?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(); ?></p>
         </div>
         <div class="generic-content">
           <?php the_excerpt(); ?>
