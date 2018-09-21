2495d7e2afeff269640ce84dc96e537ff43eee25
Asmahan Hussien <loveallah734@yahoo.com>
2018-09-21 14:33:30 +0200

    search using rest API AJAX

--- functions.php
+++ functions.php
@@ -27,18 +27,20 @@ function pageBanner($args= NULL)
   </div>
 <?php
 }
-function college_files()
+function university_files()
 {
   wp_enqueue_script('main_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('google_font',  '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font_awesome', '//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('college_main_styles', get_stylesheet_uri(), NULL, microtime());
-
+  wp_localize_script('main_js', 'universityData', array(
+    'root_url' => get_site_url()
+  ));
 }
 
-add_action('wp_enqueue_scripts', 'college_files');
+add_action('wp_enqueue_scripts', 'university_files');
 
-function college_features()
+function university_features()
 {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
@@ -46,7 +48,7 @@ function college_features()
   add_image_size('professorPortrait', 480, 650, true);
   add_image_size('pageBanner', 1500, 350, true);
 }
-add_action('after_setup_theme', 'college_features');
+add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query)
 {
